4a39abda7501c6068abee03b7a0d0829b3fca5e8
kmilo <kamil.kazmierczak@pm.me>
2023-10-21 23:31:45 +0200

    Small fixes

--- components/Container.tsx
+++ components/Container.tsx
@@ -22,9 +22,9 @@ const Container = () => {
         powerOfNicotine: 0.0,
         amountOfAroma: 0.0,
         wantedPower: 0.0,
-        calculatedGlycerin: 0.0,
-        calculatedGlycol: 0.0,
-        calculatedNicotine: 0.0,
+        glycerinFromNicotine: 0.0,
+        glycolFromNicotine: 0.0,
+        nicotine: 0.0,
         glycerin: 0.0,
         glycol: 0.0,
     }
@@ -35,14 +35,14 @@ const Container = () => {
     const calculate = () => {
         setCalc(prevState => ({
             ...prevState,
-            calculatedGlycerin: millilitersOfGlycerinPerNicotineMilligram * (prevState.amountOfLiquid * prevState.wantedPower) + (prevState.amountOfAroma * 0.7),
-            calculatedGlycol: millilitersOfGlycolPerNicotineMilligram * (prevState.amountOfLiquid * prevState.wantedPower) + (prevState.amountOfAroma * 0.3),
-            calculatedNicotine: prevState.amountOfLiquid * prevState.wantedPower / prevState.powerOfNicotine,
+            glycerinFromNicotine: millilitersOfGlycerinPerNicotineMilligram * (prevState.amountOfLiquid * prevState.wantedPower),
+            glycolFromNicotine: millilitersOfGlycolPerNicotineMilligram * (prevState.amountOfLiquid * prevState.wantedPower),
+            nicotine: prevState.amountOfLiquid * prevState.wantedPower / prevState.powerOfNicotine,
         }))
         setCalc(prevState => ({
             ...prevState,
-            glycerin: 0.7 * prevState.amountOfLiquid - prevState.calculatedGlycerin,
-            glycol: 0.3 * prevState.amountOfLiquid - prevState.calculatedGlycol,
+            glycerin: 0.7 * prevState.amountOfLiquid - prevState.glycerinFromNicotine - (prevState.amountOfAroma * 0.7),
+            glycol: 0.3 * prevState.amountOfLiquid - prevState.glycolFromNicotine - (prevState.amountOfAroma * 0.3),
         }))
 
 
@@ -64,6 +64,7 @@ const Container = () => {
                     <Input mb="$5"
                            variant="rounded">
                         <InputField
+                            keyboardType="numeric"
                             onChangeText={(value) => setCalc(prev => ({...prev, amountOfLiquid: Number(value)}))}
                             placeholder="Total amount of liquid [ml]"
                         />
@@ -74,6 +75,7 @@ const Container = () => {
                     <Input mb="$5"
                            variant="underlined">
                         <InputField
+                            keyboardType="numeric"
                             onChangeText={(value) => setCalc(prev => ({...prev, powerOfNicotine: Number(value)}))}
                             placeholder="Power of nicotine [mg/ml]"
                         />
@@ -83,6 +85,7 @@ const Container = () => {
                     </FormControlLabel>
                     <Input mb="$5">
                         <InputField
+                            keyboardType="numeric"
                             onChangeText={(value) => setCalc(prev => ({...prev, amountOfAroma: Number(value)}))}
                             placeholder="Amount of aroma [ml]"
                         />
@@ -92,6 +95,7 @@ const Container = () => {
                     </FormControlLabel>
                     <Input>
                         <InputField
+                            keyboardType="numbers-and-punctuation"
                             onChangeText={(value) => setCalc(prev => ({...prev, wantedPower: Number(value)}))}
                             placeholder="Wanted power [mg/ml]"
                         />
@@ -102,18 +106,18 @@ const Container = () => {
             <Box flex={1}>
                 <Divider padding={1} marginVertical={20}/>
 
-                <Text>Glycerin: {calc.calculatedGlycerin.toFixed(2)} ml</Text>
-                <Text>Glycol: {calc.calculatedGlycol.toFixed(2)} ml</Text>
-                <Text>Nicotine: {calc.calculatedNicotine.toFixed(2)} ml</Text>
-
+                <Text>Glycerin from nicotine: {(calc.glycerinFromNicotine).toFixed(2)} ml</Text>
+                <Text>Glycol from nicotine: {(calc.glycolFromNicotine).toFixed(2)} ml</Text>
+                <Text>Glycerin from aroma: {(0.7 * calc.amountOfAroma).toFixed(2)} ml</Text>
+                <Text>Glycol from aroma: {(0.3 * calc.amountOfAroma).toFixed(2)} ml</Text>
             </Box>
 
             <Box flex={2}>
                 <Divider padding={1} marginVertical={20}/>
 
-                <Text>Glycerin to put: {calc.glycerin.toFixed(2)} ml ({(calc.glycerin * glycerinDensity).toFixed(2)} g)</Text>
-                <Text>Glycol to put: {calc.glycol.toFixed(2)} ml ({(calc.glycol * glycolDensity).toFixed(2)} g)</Text>
-
+                <Text>Nicotine: {calc.nicotine.toFixed(2)} ml ({(calc.glycerinFromNicotine * glycerinDensity + calc.glycolFromNicotine * glycolDensity).toFixed(2)} g)</Text>
+                <Text>Glycerin: {calc.glycerin.toFixed(2)} ml ({(calc.glycerin * glycerinDensity).toFixed(2)} g)</Text>
+                <Text>Glycol: {calc.glycol.toFixed(2)} ml ({(calc.glycol * glycolDensity).toFixed(2)} g)</Text>
 
                 <Button mt="$16" onPress={calculate}>
                     <ButtonText>Calculate</ButtonText>
